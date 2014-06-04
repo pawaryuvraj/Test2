3c084f1e5c3ea1aa080d2a3287beb5dd65752498
yuvraj <pawar.yuvraj@rediffmail.com>
2014-06-04 20:42:31 +0200

    Added Comments

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -15,9 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //following are the changes to the file 
-        // TODO code application logic here
-        //Rest the head pointer
+        //New YuviTest
     }
   
 }
